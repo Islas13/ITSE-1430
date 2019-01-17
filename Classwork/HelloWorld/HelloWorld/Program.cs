5afff7380e4294ce80c53c3455fad9330a2b1608
richard.islas <richard.islas@NEITD-123504>
2019-01-16 21:43:34 -0600

    Update

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -8,9 +8,21 @@ namespace HelloWorld
 {
     class Program
     {
+        //Single line comment
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.WriteLine(name);
+
+            //String examples
+            //string message = "Hello " + name;
+            //Console.WriteLine(message);
+            //message += name;
         }
     }
 }
