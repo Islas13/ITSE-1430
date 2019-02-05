491922112da9d43b84ebb0e1a7a1a41d7afbce4a
richard.islas <richard.islas@NEITD-123912>
2019-02-04 19:18:48 -0600

    Update

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -23,30 +23,42 @@ namespace PizzaCreator
         {
             Console.WriteLine("\n(N)ew Order" + "\n(D)isplay Order" + "\n(M)odify Order" + "\nE(x)it");
             Console.WriteLine("\nPlease choose a menu option from above: ");
-            string result = Console.ReadLine(); 
+            string result = Console.ReadLine().ToUpper();
 
-            result = Console.ReadLine().ToUpper();
+            if (result == "N")
+                NewOrder();
 
-            switch (result)
-            {
-                case "N": return true;
-                case "D": return true;
-                case "M": return true;
-                case "X": return true;
+            //switch (result)
+            //{
+            //    case "N": return true;
+            //    case "D": return true;
+            //    case "M": return true;
+            //    case "X": return true;
 
-                default: return false; break;    
-            };
+            //    default: return false; break;    
+            //};
+
+            return false;
         }
 
         private static void NewOrder()
         {
-            Console.WriteLine("\nChoose size: " + "\nSmall" + "\nMedium" + "\nLarge");
-            size = Console.ReadLine();
+            Console.WriteLine("\n(S)mall  $5" + "\n(M)edium  $6.25" + "\n(L)arge  $8.25" + "\n\nChoose size: ");
+            size = Console.ReadLine().ToUpper();
 
-            
+            if (size == "S")
+                price = price + 5.0m;
+            if (size == "M")
+                price = price + 6.25m;
+            if (size == "L")
+                price = price + 8.25m;
+
+            Console.WriteLine(size);
+            Console.WriteLine(price);
         }
 
         private static string size;
         private static string sauce;
+        private static decimal price;
     }
 }
