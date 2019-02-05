8513df0af0029b7f3259175ba80b6b132c1c62c0
richard.islas <richard.islas@my.tccd.edu>
2019-02-04 21:37:06 -0600

    Update

---
+++ Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameManager.Host.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadUI();
+        }
+
+        void LoadUI()
+        {
+            // Need to add from the UI project references to use the code
+            Game game = new Game();
+
+            game.Name = "DOOM";
+            game.Price = 59.99m;
+        }
+    }
+}
